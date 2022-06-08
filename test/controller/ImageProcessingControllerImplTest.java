fb625e107769b4bd5319a0943ac61e7954bdd87d
Elvin Cheng <elvincheng3@gmail.com>
2022-06-08 18:39:20 -0400

    Added constructor tests for image processing controller

--- test/controller/ImageProcessingControllerImplTest.java
+++ test/controller/ImageProcessingControllerImplTest.java
@@ -1,8 +1,11 @@
 package controller;
 
+import model.Image;
 import model.ImageProcessingModel;
+import model.ImageProcessingModelImpl;
 import org.junit.Test;
 import view.ImageProcessingTextView;
+import view.ImageProcessingView;
 
 import java.io.StringReader;
 
@@ -57,6 +60,60 @@ public class ImageProcessingControllerImplTest {
     controller.start();
     assertEquals(expectedOutput.toString(), actualOutput.toString());
     return true;
+  }
+
+  @Test
+  public void testConstructor() {
+    Readable input = new StringReader("q\n");
+    String expectedOutput = "*** Image Processing Program ***\nEnter a command to start.\n";
+    Appendable output = new StringBuilder();
+
+    ImageProcessingModel model = new ImageProcessingModelImpl();
+    ImageProcessingView view = new ImageProcessingTextView(output);
+    ImageProcessingController controller = new ImageProcessingControllerImpl(model, view, input);
+    controller.start();
+    assertEquals(expectedOutput, output.toString());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorNullModel() {
+    ImageProcessingView view = new ImageProcessingTextView();
+    ImageProcessingController controller = new ImageProcessingControllerImpl(null, view);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorNullView() {
+    ImageProcessingModel model = new ImageProcessingModelImpl();
+    ImageProcessingController controller = new ImageProcessingControllerImpl(model, null);
+  }
+
+  @Test
+  public void testConstructorSpecifiedReadable() {
+    ImageProcessingModel model = new ImageProcessingModelImpl();
+    ImageProcessingView view = new ImageProcessingTextView();
+    Readable in = new StringReader("");
+    ImageProcessingController controller = new ImageProcessingControllerImpl(model, view, in);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorSpecifiedReadableNullModel() {
+    ImageProcessingView view = new ImageProcessingTextView();
+    Readable in = new StringReader("");
+    ImageProcessingController controller = new ImageProcessingControllerImpl(null, view, in);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorSpecifiedReadableNullView() {
+    ImageProcessingModel model = new ImageProcessingModelImpl();
+    Readable in = new StringReader("");
+    ImageProcessingController controller = new ImageProcessingControllerImpl(model, null, in);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorSpecifiedReadableNullReadable() {
+    ImageProcessingModel model = new ImageProcessingModelImpl();
+    ImageProcessingView view = new ImageProcessingTextView();
+    ImageProcessingController controller = new ImageProcessingControllerImpl(model, view, null);
   }
 
   @Test
