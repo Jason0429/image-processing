7303940270dfde3a2fe350bf4438adbd19c18466
Elvin Cheng <elvincheng3@gmail.com>
2022-06-09 09:20:38 -0400

    rewrote existing blue component tests

--- test/controller/ControllerBlueComponentTest.java
+++ test/controller/ControllerBlueComponentTest.java
@@ -17,15 +17,15 @@ import static org.junit.Assert.assertTrue;
  * Tests blue-component command for controller.
  */
 public class ControllerBlueComponentTest {
-  private Appendable appendable;
+  //  private Appendable appendable;
   private ImageProcessingModel model;
-  private ImageProcessingView view;
+//  private ImageProcessingView view;
 
   @Before
   public void init() {
-    this.appendable = new StringBuilder();
+//    this.appendable = new StringBuilder();
     this.model = new ImageProcessingModelImpl();
-    this.view = new ImageProcessingTextView(this.appendable);
+//    this.view = new ImageProcessingTextView(this.appendable);
   }
 
   @Test
@@ -55,61 +55,97 @@ public class ControllerBlueComponentTest {
 
   @Test
   public void testImageDoesNotExist() {
-    Readable readable = new StringReader(
-            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
-                    "blue-component does-not-exist test-blue" + System.lineSeparator() + "q");
-    ImageProcessingController controller = new ImageProcessingControllerImpl(
-            this.model, this.view, readable);
-    controller.start();
-    assertEquals(appendable.toString(),
-            "*** Image Processing Program ***\n" +
-                    "Enter a command to start.\n" +
-                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
-                    "Invalid parameters specified, please try again.\n");
+    assertTrue(IPCTester.testRun(this.model,
+            IPCTester.prints("*** Image Processing Program ***"),
+            IPCTester.prints("Enter a command to start."),
+            IPCTester.inputs("load test/test-images/test3x4.ppm test\n"),
+            IPCTester.prints("Successfully loaded test from test/test-images/test3x4.ppm"),
+            IPCTester.inputs("blue-component does-not-exist test-blue\n"),
+            IPCTester.prints("Invalid parameters specified, please try again."),
+            IPCTester.inputs("q\n")));
+
+//    Readable readable = new StringReader(
+//            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
+//                    "blue-component does-not-exist test-blue" + System.lineSeparator() + "q");
+//    ImageProcessingController controller = new ImageProcessingControllerImpl(
+//            this.model, this.view, readable);
+//    controller.start();
+//    assertEquals(appendable.toString(),
+//            "*** Image Processing Program ***\n" +
+//                    "Enter a command to start.\n" +
+//                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
+//                    "Invalid parameters specified, please try again.\n");
   }
 
   @Test
   public void testInvalid() {
-    Readable readable = new StringReader(
-            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
-                    "blue-component 1 2" + System.lineSeparator() + "q");
-    ImageProcessingController controller = new ImageProcessingControllerImpl(
-            this.model, this.view, readable);
-    controller.start();
-    assertEquals(appendable.toString(),
-            "*** Image Processing Program ***\n" +
-                    "Enter a command to start.\n" +
-                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
-                    "Invalid parameters specified, please try again.\n");
+    assertTrue(IPCTester.testRun(this.model,
+            IPCTester.prints("*** Image Processing Program ***"),
+            IPCTester.prints("Enter a command to start."),
+            IPCTester.inputs("load test/test-images/test3x4.ppm test\n"),
+            IPCTester.prints("Successfully loaded test from test/test-images/test3x4.ppm"),
+            IPCTester.inputs("blue-component 1 2\n"),
+            IPCTester.prints("Invalid parameters specified, please try again."),
+            IPCTester.inputs("q\n")));
+
+//    Readable readable = new StringReader(
+//            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
+//                    "blue-component 1 2" + System.lineSeparator() + "q");
+//    ImageProcessingController controller = new ImageProcessingControllerImpl(
+//            this.model, this.view, readable);
+//    controller.start();
+//    assertEquals(appendable.toString(),
+//            "*** Image Processing Program ***\n" +
+//                    "Enter a command to start.\n" +
+//                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
+//                    "Invalid parameters specified, please try again.\n");
   }
 
   @Test
   public void testLessArgs() {
-    Readable readable = new StringReader(
-            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
-                    "blue-component test" + System.lineSeparator() + "q");
-    ImageProcessingController controller = new ImageProcessingControllerImpl(
-            this.model, this.view, readable);
-    controller.start();
-    assertEquals(appendable.toString(),
-            "*** Image Processing Program ***\n" +
-                    "Enter a command to start.\n" +
-                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
-                    "Invalid parameters specified, please try again.\n");
+    assertTrue(IPCTester.testRun(this.model,
+            IPCTester.prints("*** Image Processing Program ***"),
+            IPCTester.prints("Enter a command to start."),
+            IPCTester.inputs("load test/test-images/test3x4.ppm test\n"),
+            IPCTester.prints("Successfully loaded test from test/test-images/test3x4.ppm"),
+            IPCTester.inputs("blue-component test\n"),
+            IPCTester.prints("Invalid parameters specified, please try again."),
+            IPCTester.inputs("q\n")));
+
+//    Readable readable = new StringReader(
+//            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
+//                    "blue-component test" + System.lineSeparator() + "q");
+//    ImageProcessingController controller = new ImageProcessingControllerImpl(
+//            this.model, this.view, readable);
+//    controller.start();
+//    assertEquals(appendable.toString(),
+//            "*** Image Processing Program ***\n" +
+//                    "Enter a command to start.\n" +
+//                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
+//                    "Invalid parameters specified, please try again.\n");
   }
 
   @Test
   public void testMoreArgs() {
-    Readable readable = new StringReader(
-            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
-                    "blue-component test test-green extra" + System.lineSeparator() + "q");
-    ImageProcessingController controller = new ImageProcessingControllerImpl(
-            this.model, this.view, readable);
-    controller.start();
-    assertEquals(appendable.toString(),
-            "*** Image Processing Program ***\n" +
-                    "Enter a command to start.\n" +
-                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
-                    "Invalid parameters specified, please try again.\n");
+    assertTrue(IPCTester.testRun(this.model,
+            IPCTester.prints("*** Image Processing Program ***"),
+            IPCTester.prints("Enter a command to start."),
+            IPCTester.inputs("load test/test-images/test3x4.ppm test\n"),
+            IPCTester.prints("Successfully loaded test from test/test-images/test3x4.ppm"),
+            IPCTester.inputs("blue-component test test-blue extra\n"),
+            IPCTester.prints("Invalid parameters specified, please try again."),
+            IPCTester.inputs("q\n")));
+
+//    Readable readable = new StringReader(
+//            "load test/test-images/test3x4.ppm test" + System.lineSeparator() +
+//                    "blue-component test test-blue extra" + System.lineSeparator() + "q");
+//    ImageProcessingController controller = new ImageProcessingControllerImpl(
+//            this.model, this.view, readable);
+//    controller.start();
+//    assertEquals(appendable.toString(),
+//            "*** Image Processing Program ***\n" +
+//                    "Enter a command to start.\n" +
+//                    "Successfully loaded test from test/test-images/test3x4.ppm\n" +
+//                    "Invalid parameters specified, please try again.\n");
   }
 }
