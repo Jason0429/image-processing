5e4158734f371e774d06c351fb51cb1d3aff0c5f
Elvin Cheng <elvincheng3@gmail.com>
2022-06-10 14:13:08 -0400

    added gaussian and sepia

---
+++ src/model/commands/GaussianBlurCommand.java
@@ -0,0 +1,11 @@
+package model.commands;
+
+import model.Image;
+
+public class GaussianBlurCommand implements ImageProcessingCommand {
+
+  @Override
+  public Image process(Image img) throws IllegalArgumentException {
+    return null;
+  }
+}
---
+++ src/model/commands/SepiaProcessingCommand.java
@@ -0,0 +1,24 @@
+package model.commands;
+
+import model.Pixel;
+
+/**
+ * Represents a command that produces a sepia version.
+ */
+public class SepiaProcessingCommand extends AbstractImageProcessingCommand implements ImageProcessingCommand {
+
+  /**
+   * Produces the sepia version of the pixel.
+   *
+   * @param pixel the pixel to be processed
+   * @return the sepia version of the pixel
+   * @throws IllegalArgumentException if the pixel is null
+   */
+  @Override
+  protected Pixel processPixel(Pixel pixel) throws IllegalArgumentException {
+    int red = (int) (0.393 * pixel.getRed() + 0.769 * pixel.getGreen() + 0.189 * pixel.getBlue());
+    int green = (int) (0.349 * pixel.getRed() + 0.686 * pixel.getGreen() + 0.168 * pixel.getBlue());
+    int blue = (int) (0.272 * pixel.getRed() + 0.534 * pixel.getGreen() + 0.131 * pixel.getBlue());
+    return new Pixel(pixel.getMaxValue(), red, green, blue);
+  }
+}
