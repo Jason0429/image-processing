23666e2336bdeec2095d2c26bcc44bd44b50070e
Jason Cheung <jasoncheung0429@gmail.com>
2022-06-17 15:28:06 -0400

    added transparent bmp

--- src/controller/exporter/BMPExporter.java
+++ src/controller/exporter/BMPExporter.java
@@ -1,14 +1,8 @@
 package controller.exporter;
 
-import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import model.ImageInterface;
-import model.Pixel;
 
 public class BMPExporter extends AbstractImageExporter {
   public BMPExporter(ImageInterface image, String filePath) {
