28e2412df4b6070a1c0c8cf6c9227363ae288075
Elvin Cheng <elvincheng3@gmail.com>
2022-06-20 10:27:12 -0400

    outlined tests

---
+++ test/view/GUIViewTest.java
@@ -0,0 +1,48 @@
+package view;
+
+public class GUIViewTest {
+
+  // test apply with empty image
+
+  // test apply with brighten with empty image
+
+  // test export with empty image
+
+  // test load invalid file type
+
+  // test load .ppm
+
+  // test load .png
+
+  // test load .bmp
+
+  // test load .jpeg
+
+  // test load .jpg
+
+  // test apply blue, export, and compare to actual blue
+
+  // test apply brighten, export, and compare to actual brighten
+
+  // test apply brighten darken, export, and compare to actual brighten darken
+
+  // test apply flip, export, and compare to actual flip
+
+  // test gaussian blur, export, and compare to actual gaussian blur
+
+  // test apply green, export, and compare to actual green
+
+  // test intensity, export, and compare to actual intensity
+
+  // test luma, export, and compare to actual luma
+
+  // test red, export, and compare to actual red
+
+  // test sepia, export and compare to actual sepia
+
+  // test sharpen, export, and compare to actual sharpen
+
+  // test value, export, and compare to actual value
+
+  // test loading image, applying two commands, and then exporting
+}
